ff4f85fa1122805f22beed2f0d4f8af5dade5aee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-02-22 00:09:28 +0100

    Fixed test classes.

---
+++ embedded-kafka/src/main/java/com/github/starnowski/kafka/fun/KafkaProducer.java
@@ -0,0 +1,16 @@
+package com.github.starnowski.kafka.fun;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload) {
+        kafkaTemplate.send(topic, payload);
+    }
+}
