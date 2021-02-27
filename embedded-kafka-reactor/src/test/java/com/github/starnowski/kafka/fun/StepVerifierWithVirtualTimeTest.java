ab942bb7752eb607e96cce61980469810846d3cf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-02-28 00:31:07 +0100

    Added tests cases.

--- embedded-kafka-reactor/src/test/java/com/github/starnowski/kafka/fun/StepVerifierWithVirtualTimeTest.java
+++ embedded-kafka-reactor/src/test/java/com/github/starnowski/kafka/fun/StepVerifierWithVirtualTimeTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.ReceiverRecord;
 import reactor.test.StepVerifier;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
+import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -27,11 +29,13 @@ public class StepVerifierWithVirtualTimeTest {
         StepVerifier
                 .withVirtualTime(() -> Flux.just(receiverRecord)
                         .flatMap(rr -> supplierWithFailerHandler.get(rr))
-                        .retryWhen(Retry.backoff(1, Duration.ofSeconds(2)))
+                        .log()
+                        .retryWhen(Retry.backoff(1, ofSeconds(2)))
+                        .log()
                 )
                 .expectSubscription()
-                .expectNoEvent(Duration.ofSeconds(2))
-                .thenAwait(Duration.ofSeconds(1))
+                .expectNoEvent(ofSeconds(2))
+                .thenAwait(ofSeconds(1))
                 .expectNext(13)
                 .verifyComplete();
 
@@ -48,11 +52,12 @@ public class StepVerifierWithVirtualTimeTest {
         StepVerifier
                 .withVirtualTime(() -> Flux.just(receiverRecord).flatMap(rr -> supplierWithFailerHandler.get(rr)
                         )
-                                .retryWhen(Retry.backoff(1, Duration.ofSeconds(2)))
+                                .retryWhen(Retry.backoff(1, ofSeconds(2)))
+                        .log()
                 )
                 .expectSubscription()
-                .expectNoEvent(Duration.ofSeconds(2))
-                .thenAwait(Duration.ofSeconds(1))
+                .expectNoEvent(ofSeconds(2))
+                .thenAwait(ofSeconds(1))
                 .verifyError(RuntimeException.class);
         assertEquals(2, supplierWithFailerHandler.getCurrent());
     }
@@ -71,15 +76,17 @@ public class StepVerifierWithVirtualTimeTest {
         StepVerifier
                 .withVirtualTime(() -> Flux.just(receiverRecord1, receiverRecord2).flatMap(rr -> supplierWithFailerHandler.get(rr)
                         )
+                        .log()
                                 .retryWhen(
                                         Retry
-                                                .backoff(1, Duration.ofSeconds(2))
+                                                .backoff(1, ofSeconds(2))
                                                 .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
 
                                                     return new RuntimeException("AAA");
                                                 })
                                                 .transientErrors(true)
                                 )
+                                .log()
                 )
                 .expectSubscription()
                 .expectNext(13)
@@ -90,9 +97,49 @@ public class StepVerifierWithVirtualTimeTest {
     }
 
     @Test
+    public void shouldProcessAllEventsEvenWhenFirstAttemptForFirstEventFails() {
+        // GIVEN
+        ConstantNumberSupplierWithFailerHandler supplierWithFailerHandler = mock(ConstantNumberSupplierWithFailerHandler.class);
+        ReceiverRecord<String, String> receiverRecord1 = mock(ReceiverRecord.class);
+        ReceiverRecord<String, String> receiverRecord2 = mock(ReceiverRecord.class);
+        when(supplierWithFailerHandler.get(receiverRecord1)).thenThrow(new RuntimeException("1234")).thenReturn(Flux.just(13));
+        when(supplierWithFailerHandler.get(receiverRecord2)).thenReturn(Flux.just(45));
+
+
+        // THEN
+        StepVerifier
+                .withVirtualTime(() -> Flux.just(receiverRecord1, receiverRecord2).flatMap(rr -> supplierWithFailerHandler.get(rr)
+                        )
+                                .log()
+                                .retryWhen(
+                                        Retry
+                                                .backoff(1, ofSeconds(2))
+                                                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
+
+                                                    return new RuntimeException("AAA");
+                                                })
+                                                .transientErrors(true)
+                                )
+                                .log()
+                )
+                .expectSubscription()
+                .expectNoEvent(Duration.ofSeconds(2))
+                .thenAwait(ofSeconds(2))
+                .thenAwait(ofSeconds(2))
+                .expectNext(13)
+                .expectNext(45)
+                .verifyComplete();
+        verify(supplierWithFailerHandler, times(2)).get(receiverRecord1);
+        verify(supplierWithFailerHandler, atMostOnce()).get(receiverRecord2);
+    }
+
+
+    @Test
     @Disabled("Not yet finished")
     public void xxx() {
         // GIVEN
+        // https://www.vinsguru.com/reactor-repeat-vs-reactor-retry/
+        // https://blog.trifork.com/2019/03/13/retry-functionality-in-a-reactive-programming-context/
         ConstantNumberSupplierWithFailerHandler supplierWithFailerHandler = mock(ConstantNumberSupplierWithFailerHandler.class);
         ReceiverRecord<String, String> receiverRecord1 = mock(ReceiverRecord.class);
         ReceiverRecord<String, String> receiverRecord2 = mock(ReceiverRecord.class);
@@ -102,35 +149,33 @@ public class StepVerifierWithVirtualTimeTest {
 
         // THEN
         StepVerifier
-                .withVirtualTime(() -> Flux.just(receiverRecord1, receiverRecord2).flatMap(rr -> supplierWithFailerHandler.get(rr)
-//                                .retryWhen(Retry.backoff(1, Duration.ofSeconds(8)))
-                        )
-                        .retryWhen(
-                                Retry
-                                        .backoff(1, Duration.ofSeconds(2))
-                                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> {
-
-                                            return new RuntimeException("AAA");
-                                        })
+                .withVirtualTime(() -> Flux.just(receiverRecord1, receiverRecord2).flatMap(
+                        rr ->
+//                                supplierWithFailerHandler.get(rr)
+//                                .retryWhen(Retry
+//                                        .backoff(1, Duration.ofSeconds(2))
+//                                        .transientErrors(true)
+//                                )
+//                                .log()
+//                                .onErrorReturn(-1)
+                        Mono.just(supplierWithFailerHandler.get(rr))
+                                                        .retryWhen(Retry
+                                        .backoff(1, ofSeconds(2))
                                         .transientErrors(true)
+                                )
+                                .log().block()
+
                         )
-//                                .onErrorContinue(throwable -> {
-//                                    System.out.println(throwable);
-//                                    return RuntimeException.class.equals(throwable.getClass()) && "AAA".equals(throwable.getMessage());
-//                                }, (throwable, o) -> {
-//
-//                                })
-                                .onErrorReturn(throwable -> {
-                                            System.out.println(throwable);
-                                            return RuntimeException.class.equals(throwable.getClass()) && "AAA".equals(throwable.getMessage());
-                                        },
-                                        -1
-                                        )
+                        .onErrorContinue(throwable -> true, (throwable, o) -> {})
+                        .log()
+
                 )
                 .expectSubscription()
-                .expectNoEvent(Duration.ofSeconds(2))
-                .thenAwait(Duration.ofSeconds(2))
-                .expectNext(-1)
+//                .expectNoEvent(Duration.ofSeconds(2))
+                .thenAwait(ofSeconds(2))
+//                .thenAwait(Duration.ofSeconds(2))
+//                .expectNext(-1)
+//                .thenAwait(Duration.ofSeconds(2))
                 .expectNext(45)
                 .verifyComplete();
         verify(supplierWithFailerHandler, times(2)).get(receiverRecord1);
