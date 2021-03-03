39238b744f9f40cdfa650d1c274d204530436c0f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-03 21:48:30 +0100

    Tests code refactoring.

--- embedded-kafka-reactor/src/main/java/com/github/starnowski/kafka/fun/ConstantNumberSupplierWithFailerHandler.java
+++ embedded-kafka-reactor/src/main/java/com/github/starnowski/kafka/fun/ConstantNumberSupplierWithFailerHandler.java
@@ -1,31 +1,35 @@
 package com.github.starnowski.kafka.fun;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.ReceiverRecord;
-
-import java.util.Random;
 
 public class ConstantNumberSupplierWithFailerHandler {
 
     private final int maxAttempt;
-
-    public int getCurrent() {
-        return current;
-    }
-
     private int current = 0;
 
     public ConstantNumberSupplierWithFailerHandler(int maxAttempt) {
         this.maxAttempt = maxAttempt;
     }
 
-    public Flux<Integer> get(ReceiverRecord<String, String> receiverRecord)
-    {
+    public int getCurrent() {
+        return current;
+    }
+
+    public Flux<Integer> get(ReceiverRecord<String, String> receiverRecord) {
         current++;
-        if (current < maxAttempt)
-        {
+        if (current < maxAttempt) {
             throw new RuntimeException("xxx");
         }
         return Flux.just(13);
+    }
+
+    public Mono<Integer> getMono(ReceiverRecord<String, String> receiverRecord) {
+        current++;
+        if (current < maxAttempt) {
+            throw new RuntimeException("xxx");
+        }
+        return Mono.just(13);
     }
 }
