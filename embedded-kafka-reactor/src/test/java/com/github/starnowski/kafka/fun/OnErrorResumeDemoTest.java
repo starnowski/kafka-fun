db944c3db581ca4b8590c962b2c72b05721f8de8
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-03 23:45:03 +0200

    Added tests

---
+++ embedded-kafka-reactor/src/test/java/com/github/starnowski/kafka/fun/OnErrorResumeDemoTest.java
@@ -0,0 +1,27 @@
+package com.github.starnowski.kafka.fun;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class OnErrorResumeDemoTest {
+
+    @Test
+    public void firstTestCase() {
+        // GIVEN
+        Flux<Integer> stream = Flux.range(1, 5)
+                .doOnNext(i -> System.out.println("input=" + i))
+                .map(i -> i == 3 ? i / 0 : i)
+                .map(i -> i);
+
+        // WHEN
+        StepVerifier.FirstStep<Integer> stepVerifier = StepVerifier.create(stream);
+
+        // THEN
+        stepVerifier
+                .expectNext(1)
+                .expectNext(2)
+                .expectError(ArithmeticException.class)
+                .verify();
+    }
+}
