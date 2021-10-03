74e3d53b26fe45c6099291f8027394e84873b31b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-03 23:48:20 +0200

    Added second tests case

--- embedded-kafka-reactor/src/test/java/com/github/starnowski/kafka/fun/OnErrorResumeDemoTest.java
+++ embedded-kafka-reactor/src/test/java/com/github/starnowski/kafka/fun/OnErrorResumeDemoTest.java
@@ -24,4 +24,26 @@ public class OnErrorResumeDemoTest {
                 .expectError(ArithmeticException.class)
                 .verify();
     }
+
+    @Test
+    public void secondTestCase() {
+        // GIVEN
+        Flux<Integer> stream = Flux.range(1, 5)
+                .doOnNext(i -> System.out.println("input=" + i))
+                .map(i -> i == 3 ? i / 0 : i)
+                .map(i -> i)
+                .onErrorResume(err -> {
+                    System.out.println("onErrorResume");
+                    return Flux.empty();
+                });
+
+        // WHEN
+        StepVerifier.FirstStep<Integer> stepVerifier = StepVerifier.create(stream);
+
+        // THEN
+        stepVerifier
+                .expectNext(1)
+                .expectNext(2)
+                .verifyComplete();
+    }
 }
